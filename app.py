29eaf00b5616e09d66fbd4aaf33499707fdbbd45
Jordan Tannenbaum <jordantannen@gmail.com>
2022-05-01 22:11:42 -0400

    Ensure user can only input audio files. Show error when this is not done.

--- app.py
+++ app.py
@@ -13,8 +13,15 @@ UPLOAD_FOLDER = '/home/jordan/CS50/stem-player/uploads/'
 STEM_FOLDER = ''
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+ALLOWED_EXTENSIONS = {'wav', 'mp3'}
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensures file is correct type
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Stop cache
 @app.after_request
@@ -30,28 +37,39 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # Create new spleeter object
-        separator = Separator('spleeter:4stems')
+        # Ensure proper file was uploaded
+        if 'file' not in request.files:
+            return render_template("index.html", error_message=True)
 
-        # Save file to variable
+        # Save song to variable
         song = request.files["file"]
-        filename = secure_filename(song.filename)
 
-        # Set save path and save file
-        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        song.save(audio_path)
+        # Ensure file was uploaded
+        if song.filename == "":
+            return render_template("index.html", error_message=True)
 
-        # Split song and save to path
-        separator.separate_to_file(audio_path, UPLOAD_FOLDER)
+        if song and allowed_file(song.filename):
+            # Create new spleeter object
+            separator = Separator('spleeter:4stems')
 
-        # Set location for stems
-        global STEM_FOLDER
-        STEM_FOLDER = UPLOAD_FOLDER + str(filename).replace(".mp3", "")
+            # Save create secure filename
+            filename = secure_filename(song.filename)
 
-        # File list for stem output 
-        file_list = ["bass.wav", "drums.wav", "other.wav", "vocals.wav"]
+            # Set save path and save file
+            audio_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            song.save(audio_path)
 
-        return render_template("player.html", file_list=file_list)
+            # Split song and save to path
+            separator.separate_to_file(audio_path, UPLOAD_FOLDER)
+
+            # Set location for stems
+            global STEM_FOLDER
+            STEM_FOLDER = UPLOAD_FOLDER + str(filename).replace(".mp3", "")
+
+            # File list for stem output 
+            file_list = ["bass.wav", "drums.wav", "other.wav", "vocals.wav"]
+
+            return render_template("player.html", file_list=file_list)
 
     else:
         return render_template("index.html")
